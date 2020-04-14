53ae42c66f88956696f639de3fe8719efceca5e4
Zairoangelo <35569226+yuuyakizami@users.noreply.github.com>
2020-04-15 06:42:43 +0800

    hello new form

---
+++ process.php
@@ -0,0 +1,29 @@
+<?php
+//Get value pass from form in login.php get_included_files
+$username = $_POST['user'];
+$password = $_POST['pass'];
+
+//to prevent mysql injection
+
+$username = stripslashes($username);
+$password = stripslashes($password);
+$username = mysql_real_escape_string($username);
+$password = mysql_real_escape_string($password);
+
+// connect to the server and select SQLiteDatabase
+mysql_connect("localhost", "root", "");
+mysql_select_db("login");
+
+// Qury the databse for user
+
+$result = mysql_query("select * from users where username = '$username' and password ='$password'")
+        or die("failed to query database ".mysql_error());
+$row = mysql_fetch_array($result);
+if($row['username'] == $username && $row['password'] == $password ){
+echo "Login success!!! Welcome ".$row['username'];
+} else {
+echo "Failed to login!";
+}
+
+
+ ?>
