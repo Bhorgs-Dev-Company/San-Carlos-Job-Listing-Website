b244bdf20875ffa28de1661b98b4375b470076e3
Zairoangelo <35569226+yuuyakizami@users.noreply.github.com>
2020-04-15 12:00:15 +0800

    yow

---
+++ 2nd Attempt/homepage.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<title>HTML Tutorial</title>
+<head>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body style="background-color:powderblue">
+<header>
+<h1>Homepage</h1>
+
+</header>
+<nav>
+  <nav>
+   <a href="/html/">HTML</a> |
+   <a href="/css/">CSS</a> |
+   <a href="/js/">JavaScript</a> |
+   <a href="/jquery/">jQuery</a>
+ </nav>
+</nav>
+<section>
+  <picture>
+   <source srcset="personal1.jpg" media="(max-width: 600px)">
+   <source srcset="personal1.jpg" media="(max-width: 1500px)">
+   <source srcset="personal1.jpg">
+   <img src="personal1.jpg" alt="Flowers">
+ </picture>
+</section>
+<footer>
+
+</footer>
+</body>
+</html>
