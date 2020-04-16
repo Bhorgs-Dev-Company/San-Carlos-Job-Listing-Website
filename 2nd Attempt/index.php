98f24bc0a53d4ff1f80ae123168a50a668ad84ba
Zairoangelo <35569226+yuuyakizami@users.noreply.github.com>
2020-04-16 17:29:03 +0800

    new

---
+++ 2nd Attempt/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>My HomePage</title>
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
+    <h1>Welcome to my Webpage!</h1>
+    <p>Hello There!</p>
+    <div>
+  Presidents
+  <table>
+    <tr>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Year's in Office</th>
+    </tr>
+    <tr>
+      <td>George</td>
+      <td>Washington</td>
+      <td>1789-1797</td>
+    </tr>
+    <tr>
+      <td>George</td>
+      <td>Washington</td>
+      <td>1789-1797</td>
+    </tr>
+    <tr>
+      <td>George</td>
+      <td>Washington</td>
+      <td>1789-1797</td>
+    </tr>
+  </table>
+    </div>
+    <div id="middle">
+      This is the <span class="name">middle</span>of my page.
+    </div>
+    <div id="bottom">
+      This is the <span class="name">bottom</span>of my page.
+    </div>
+
+  </body>
+</html>
